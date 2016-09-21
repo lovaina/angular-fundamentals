e25c1a3c2fee835d65d7be70c1a705e64eb786cb
MeiAlbiol <meialbiol@gmail.com>
2016-09-21 19:33:03 +0000

    Demo: Displaying Repeating Information

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -11,6 +11,29 @@ function EventController($scope) {
                 city: 'Mountain View',
                 province: 'CA'
             },
-            imageUrl: '/img/angularjs-logo.png'
+            imageUrl: '/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Flavum, peritus humani generiss etiam perdere de rusticus, lotus luna.',
+                    creatorName: 'Candidatuss experimentum! ',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'Rumor messiss, tanquam regius barcas.'
+                },
+                {
+                    name: 'Powers, moons, and beloved explosion of the minerals will always protect them.',
+                    creatorName: 'Amicitias resistere! ',
+                    duration: '1 hr',
+                    level: 'Introductory',
+                    abstract: 'A falsis, advena fortis navis.'
+                },
+                {
+                    name: 'Margerine can be decorateed with sweet steak, also try covering the frittata with peanut sauce.',
+                    creatorName: 'Sagas crescere! ',
+                    duration: '1 hr',
+                    level: 'Intermediate',
+                    abstract: 'Galatae potuss, tanquam magnum idoleum.'
+                }
+            ]
         }
 });
