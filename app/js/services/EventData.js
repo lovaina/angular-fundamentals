c113122dd3deeb72d6e69fbcaacf1c98e5026363
lovaina <meialbiol@gmail.com>
2016-09-26 20:13:04 +0200

    My first Service EvenData

---
+++ app/js/services/EventData.js
@@ -0,0 +1,51 @@
+eventsApp.factory('eventData', function () {
+   return {
+        event: {
+            name: 'Angular Boot Camp',
+            date: '1/1/2013',
+            time: '10:30 am',
+            location: {
+                address: 'Google headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            },
+            imageUrl: '/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Flavum, peritus humani generiss etiam perdere de rusticus, lotus luna.',
+                    creatorName: 'Candidatuss experimentum! ',
+                    duration: 1,
+                    level: 'Advanced',
+                    abstract: 'Rumor messiss, tanquam regius barcas.',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Powers, moons, and beloved explosion of the minerals will always protect them.',
+                    creatorName: 'Amicitias resistere! ',
+                    duration: 2,
+                    level: 'Introductory',
+                    abstract: 'A falsis, advena fortis navis.',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Margerine can be decorateed with sweet steak, also try covering the frittata with peanut sauce.',
+                    creatorName: 'Sagas crescere! ',
+                    duration: 3,
+                    level: 'Intermediate',
+                    abstract: 'Galatae potuss, tanquam magnum idoleum.',
+                    upVoteCount: 0
+                },
+
+                {
+                    name: 'Part 0: Margerine can be decorateed with sweet steak, also try covering the frittata with peanut sauce.',
+                    creatorName: 'Sagas crescere! ',
+                    duration: 4,
+                    level: 'Introductory',
+                    abstract: 'Galatae potuss, tanquam magnum idoleum.',
+                    upVoteCount: 0
+                }
+            ]
+
+        }
+   }
+});
