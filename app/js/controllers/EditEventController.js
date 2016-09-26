14010c95edf006ea9c7702d47d9144dbc4dd2a6b
lovaina <meialbiol@gmail.com>
2016-09-26 19:57:01 +0200

    Validation Forms

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -2,8 +2,10 @@
 
 eventsApp.controller('EditEventController',
 function EditEventController($scope) {
-    $scope.saveEvent = function (event) {
-        window.alert(event.name);
+    $scope.saveEvent = function (event, newEventForm) {
+       if (newEventForm.$valid){
+           window.alert(event.name);
+       }
     };
 
     $scope.cancelEdit = function () {
